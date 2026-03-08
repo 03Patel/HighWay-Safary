edad4498d0773a0d702d8180c24116c73e1df89f
Ganesh  ji patel <ganeshjipatel108@gmail.com>
2026-03-08 10:02:31 +0530

    h

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,17 +1,14 @@
 import axios from "axios";
 
-<<<<<<< HEAD
+
 const baseURL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : "https://mealmapbackend.onrender.com/api";
+    : "";
 
 const API = axios.create({
   baseURL,
 });
 
 export default API;
-=======
-const API = axios.create({ baseURL:'https://highway-safary.onrender.com/api' })
-export default API
->>>>>>> bfc14ec6cfc149ee9858efe52b57617c0cf680b3
+
